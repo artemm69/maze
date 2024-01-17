57e7180507ae419a0f1f276433ed438289353a2e
eg18f <156822631+eg18f@users.noreply.github.com>
2024-01-17 22:43:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ import pygame
 import random
 # импортируем библиотеку time
 import time
-# вроде я могу чтото добавлять
-# ааа капец круто прикольно 
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Оранжевый цвет по умолчанию для лабиринта
+current_maze_color = (255, 165, 0)
+# Фиолетовый цвет по умолчанию для кубика
+current_cube_color = (128, 0, 128)
